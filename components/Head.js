538d28e570e9b60246cb1f9fc64c07d69a887e25
3e <soulwawa85@gmail.com>
2019-06-29 21:04:56 +0900

    fix gtag

--- components/Head.js
+++ components/Head.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NextHead from 'next/head'
+import { GA_TRACKING_ID } from './gtag'
 
 const Head = () => (
     <NextHead>
@@ -19,12 +20,20 @@ const Head = () => (
             rel="stylesheet"
         />
         <title>3E</title>
-        <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','UA-131636335-4');`}} />
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `
+            }}
+        />
     </NextHead>
 )
 
