d4dc818328188a18157d2684aa003a8f2933418d
soulwawa <soulwawa85@gmail.com>
2019-10-04 22:59:48 +0900

    aws 설정 세팅

--- next.config.js
+++ next.config.js
@@ -1,23 +1,21 @@
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    target: 'serverless',
-    exportPathMap: function () {
-        return {
-            '/': { page: '/' },
-            '/about': { page: '/about' }
-        }
-    },
-    webpack: (config) => {
-        config.module.rules.push(
-            {
-                test: /\.md$/,
-                use: 'raw-loader'
-            }
-        )
+	exportPathMap: function() {
+		return {
+			'/': { page: '/' },
+			'/about': { page: '/about' },
+			'/projects': { page: '/projects' },
+		};
+	},
+	webpack: config => {
+		config.module.rules.push({
+			test: /\.md$/,
+			use: 'raw-loader',
+		});
 
-        return config
-    },
-}
+		return config;
+	},
+};
 
 // assetPrefix: !debug ? 'https://soulwawa.github.io/3e/' : '/',
