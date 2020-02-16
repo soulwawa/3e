9274c55437e794e31e62a401e0a9ff06be7a55df
3e <soulwawa85@gmail.com>
2020-02-16 14:23:04 +0900

    gtag except localhost

--- pages/_document.js
+++ pages/_document.js
@@ -21,10 +21,12 @@ class MyDocument extends Document {
 							__html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+            if(window.location.hostname !== 'localhost') {
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            }
           `
 						}}
 					/>
