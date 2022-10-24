a5cb23fea397f78444eb20dffd42139189df59d3
soulwawa <soulwawa85@gmail.com>
2022-10-24 22:53:01 +0900

    fix: 경력 오타 수정

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {Divider1x, LightLineDivider} from "./common";
+import { Divider1x, LightLineDivider } from "./common";
 import device from "../lib/device";
 
 const WorkContainer = styled.section`
@@ -26,7 +26,6 @@ const WorkTitle = styled.h2`
   @media ${device.mobileL} {
     font-size: 1rem;
   }
-
 `;
 
 const WorkDescriptionContainer = styled.div`
@@ -69,44 +68,48 @@ export default function Work() {
     <WorkContainer>
       <Title>👨🏻‍💻 Work Experiences</Title>
       {/*바로팜*/}
-      <Divider1x/>
-      <WorkTitle>[📃 2022.08 ~  / 바로팜 / 백엔드 엔지니어]</WorkTitle>
+      <Divider1x />
+      <WorkTitle>[📃 2022.08 ~ / 바로팜 / 백엔드 엔지니어]</WorkTitle>
       <WorkDescriptionContainer>
-        <WorkDescriptionTitle>▶️ 의약품 주문 통합 플랫폼 백엔드 엔지니어</WorkDescriptionTitle>
+        <WorkDescriptionTitle>
+          ▶️ 의약품 주문 통합 플랫폼 백엔드 엔지니어
+        </WorkDescriptionTitle>
       </WorkDescriptionContainer>
-      <Divider1x/>
+      <Divider1x />
       <WorkDescriptionDetail>
-        {
-          `- 바로팜 Restful API 설계 및 개발
-           - 파트너, CMS 페이지 개발`
-        }
+        {`- 바로팜 Restful API 설계 및 개발
+          - 파트너, CMS 페이지 개발`}
       </WorkDescriptionDetail>
-      <Divider1x/>
-      <LightLineDivider/>
+      <Divider1x />
+      <LightLineDivider />
       {/*라이픽*/}
-      <Divider1x/>
-      <WorkTitle>[📃 2022.06 ~  / 라이픽 / 테크챕터 검색&데이터 파트 / 데이터 엔지니어]</WorkTitle>
+      <Divider1x />
+      <WorkTitle>
+        [📃 2022.06 ~ 2022.07 / 라이픽 / 테크챕터 검색&데이터 파트 / 데이터
+        엔지니어]
+      </WorkTitle>
       <WorkDescriptionContainer>
-        <WorkDescriptionTitle>▶️ 올인원 QOL(Quality of Life) 플랫폼</WorkDescriptionTitle>
+        <WorkDescriptionTitle>
+          ▶️ 올인원 QOL(Quality of Life) 플랫폼
+        </WorkDescriptionTitle>
       </WorkDescriptionContainer>
-      <Divider1x/>
+      <Divider1x />
       <WorkDescriptionDetail>
-        {
-          `- 비즈니스 요구사항에 따른 데이터 처리 및 조회 시스템 설계, 개발 및 운영`
-        }
+        {`- 비즈니스 요구사항에 따른 데이터 처리 및 조회 시스템 설계, 개발 및 운영`}
       </WorkDescriptionDetail>
-      <Divider1x/>
-      <LightLineDivider/>
+      <Divider1x />
+      <LightLineDivider />
       {/*놀이의발견*/}
-      <Divider1x/>
-      <WorkTitle>[📃 2020.10 ~ 2022.06 / 놀이의발견 / 플랫폼실 백엔드파트 / 파트장 ]</WorkTitle>
+      <Divider1x />
+      <WorkTitle>
+        [📃 2020.10 ~ 2022.06 / 놀이의발견 / 플랫폼실 백엔드파트 / 파트장 ]
+      </WorkTitle>
       <WorkDescriptionContainer>
         <WorkDescriptionTitle>▶️ O2O 키즈플랫폼</WorkDescriptionTitle>
       </WorkDescriptionContainer>
-      <Divider1x/>
+      <Divider1x />
       <WorkDescriptionDetail>
-        {
-          `[O2O 키즈 플랫폼 플랫폼실 백엔드파트 매니지먼트]
+        {`[O2O 키즈 플랫폼 플랫폼실 백엔드파트 매니지먼트]
           - 2021.12 ~ 2022.06: O2O 키즈플랫폼 플랫폼실 백엔드파트 매니지먼트
           - 2021.06 ~ 2021.12 : O2O 키즈플랫폼 플랫폼실 백엔드2파트(CRM, 사장님, 정산, DevOps) 매니지먼트
           
@@ -124,78 +127,88 @@ export default function Work() {
           - 기존 jquery기반 레거시 프로젝트를 React기반으로 마이그레이션
           - 컨테이너 기반 빌드/배포 자동화 시스템 구축 참여
           - 코드레벨 최적화 및 리팩토링 다수 수행
-          - 운영 환경 : AWS ( EC2, RDS, APM)`
-        }
+          - 운영 환경 : AWS ( EC2, RDS, APM)`}
       </WorkDescriptionDetail>
-      <Divider1x/>
-      <LightLineDivider/>
+      <Divider1x />
+      <LightLineDivider />
       {/* 알고랩 */}
-      <Divider1x/>
+      <Divider1x />
       <WorkTitle>[📃 2019.07 ~ 2020.10 / 알고랩 / 개발팀 / 선임 ]</WorkTitle>
       <WorkDescriptionContainer>
-        <WorkDescriptionTitle>▶️ 라스트마일 물류 웹서비스 개발</WorkDescriptionTitle>
+        <WorkDescriptionTitle>
+          ▶️ 라스트마일 물류 웹서비스 개발
+        </WorkDescriptionTitle>
       </WorkDescriptionContainer>
-      <Divider1x/>
+      <Divider1x />
       <WorkDescriptionDetail>
-        {
-          `- Python ( Django ) 을 이용한 Rest API, GraphQL 개발
+        {`- Python ( Django ) 을 이용한 Rest API, GraphQL 개발
           - Vue, React 를 이용한 웹, 관리자 페이지 개발
           - Sanic, Peewee 를 이용한 마이크로서비스 개발
           - Hasura, Graphene-Django 를 활용한 GraphQL 개발
-          - AWS 운영 관리 : EC2(docker), RDS(Postgresql), CodePipeline, S3, ELK`
-        }
+          - AWS 운영 관리 : EC2(docker), RDS(Postgresql), CodePipeline, S3, ELK`}
       </WorkDescriptionDetail>
-      <Divider1x/>
-      <LightLineDivider/>
+      <Divider1x />
+      <LightLineDivider />
       {/* 시스메이트 */}
-      <Divider1x/>
+      <Divider1x />
       <WorkTitle>
         [📃 2019.02 ~ 2019.07 / 시스메이트 / 솔루션사업부 개발팀 / 사원 ]
       </WorkTitle>
       <WorkDescriptionContainer>
-        <WorkDescriptionTitle>▶️ 디지털사이니지 웹 솔루션 개발</WorkDescriptionTitle>
+        <WorkDescriptionTitle>
+          ▶️ 디지털사이니지 웹 솔루션 개발
+        </WorkDescriptionTitle>
       </WorkDescriptionContainer>
-      <Divider1x/>
+      <Divider1x />
       <WorkDescriptionDetail>
-        {
-          `- 디지털 사이니지 를 이용한 웹 빌더 개발 ( Django, React )
+        {`- 디지털 사이니지 를 이용한 웹 빌더 개발 ( Django, React )
           - 디지털 사이니지를 이용한 맞춤형 웹개발
           - Python ( Django ) 을 이용한 웹 개발
           - Django Admin 커스텀 마이징을 통한 관리자 페이지 개발 
-          - javascript 를 이용한 길찾기 (SVG) 개발`
-        }
+          - javascript 를 이용한 길찾기 (SVG) 개발`}
       </WorkDescriptionDetail>
-      <Divider1x/>
-      <LightLineDivider/>
+      <Divider1x />
+      <LightLineDivider />
       {/* 디플랫폼 */}
-      <Divider1x/>
-      <WorkTitle>[📃 2018.06 ~ 2019.01 / 디플랫폼 / 개발팀 / 연구원 ]</WorkTitle>
+      <Divider1x />
+      <WorkTitle>
+        [📃 2018.06 ~ 2019.01 / 디플랫폼 / 개발팀 / 연구원 ]
+      </WorkTitle>
       <WorkDescriptionContainer>
         <WorkDescriptionTitle>
           ▶️ CTI 정보보안 데이터 웹 플랫폼 구축
         </WorkDescriptionTitle>
       </WorkDescriptionContainer>
-      <Divider1x/>
+      <Divider1x />
       <WorkDescriptionDetail>
-        {
-          `- Python ( Django ) 을 이용한 사이버 위협 정보 웹 크롤링, 데이터 파싱
+        {`- Python ( Django ) 을 이용한 사이버 위협 정보 웹 크롤링, 데이터 파싱
           - MongoDB, Neo4j, PostgreSQL 를 이용한 데이터베이스 개발
           - 사이버 위협 정보 수집 시스템 운영
-          - 국가 차원의 침해사고 대응을 위한 사이버 위협 인텔리전스 분석(CTI) 및 정보 공유 기술 개발`
-        }
+          - 국가 차원의 침해사고 대응을 위한 사이버 위협 인텔리전스 분석(CTI) 및 정보 공유 기술 개발`}
       </WorkDescriptionDetail>
-      <Divider1x/>
-      <LightLineDivider/>
+      <Divider1x />
+      <LightLineDivider />
 
-      <Divider1x/>
+      <Divider1x />
       <Title>👨🏻‍💻 Education</Title>
-      <EduPrizeTitle>▶️ 2017.12 ~ 2018.04 한국생산성본부 센서네트워크 기반의 IoT 융합서비스 개발</EduPrizeTitle>
-      <EduPrizeTitle> ▶️ 2012.03 ~ 2013.03 학점은행제 컴퓨터공학 졸업 </EduPrizeTitle>
-      <EduPrizeTitle> ▶️ 2004.03 ~ 2011.03 경기대학교 건축학과 중퇴 </EduPrizeTitle>
+      <EduPrizeTitle>
+        ▶️ 2017.12 ~ 2018.04 한국생산성본부 센서네트워크 기반의 IoT 융합서비스
+        개발
+      </EduPrizeTitle>
+      <EduPrizeTitle>
+        {" "}
+        ▶️ 2012.03 ~ 2013.03 학점은행제 컴퓨터공학 졸업
+      </EduPrizeTitle>
+      <EduPrizeTitle>
+        {" "}
+        ▶️ 2004.03 ~ 2011.03 경기대학교 건축학과 중퇴
+      </EduPrizeTitle>
 
-      <Divider1x/>
+      <Divider1x />
       <Title>🏆 Prize</Title>
-      <EduPrizeTitle>▶️ 2017.12 ~ 2018.04 한국생산성본부 센서네트워크 기반의 IoT 융합서비스</EduPrizeTitle>
+      <EduPrizeTitle>
+        ▶️ 2017.12 ~ 2018.04 한국생산성본부 센서네트워크 기반의 IoT 융합서비스
+      </EduPrizeTitle>
     </WorkContainer>
   );
 }
